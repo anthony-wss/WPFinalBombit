1e1b20410fc2822cb3dc8c83a153d5738a21ad66
Anthony Wang <anthony@m62193.m6.ntu.edu.tw>
2022-01-13 11:17:06 +0800

    server.py add rule

--- On_server/server.py
+++ On_server/server.py
@@ -2,15 +2,239 @@ import asyncio
 import json
 import websockets
 import time
+from math import round
+from random import randint
 # receive : json.loads(message)
 # 收到一個dict物件 {player_id:Int,key:String}
 # 會呼叫 player_control(D) 傳入上述物件
-# 欲傳回一個dict物件 {Map:[[...],...],pos:[[x1,y1],[x2,y2],...],...}
-# 算好地圖後 呼叫boardcast_status(D)傳入上述物件 就會在所有client端收到該物件 由client渲染畫面
+# 欲傳回一個 GameState 物件 {Map:[[...],...],pos:[[x1,y1],[x2,y2],...],...}
+# 算好地圖後 呼叫boardcast_status(getGameState())傳入上述物件 就會在所有client端收到該物件 由client渲染畫面
+
+# TODO: 炸彈也要判成障礙物
+
+t = 0
+UNIT, POWER = 37, 7
+
+def idx(x):
+    return round((x*2+UNIT)/UNIT/2)
+
+class Player():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.is_moving = [0] * 5
+        self.speed = 3
+
+class Bomb():
+    def __init__(self, player_id, x, y, t):
+        self.x = x
+        self.y = y
+        self.owner = player_id
+        self.explode_time = t+1.5
+        self.set_fire = 0
+        self.fires = []
+        self.fire_time = self.explode_time + 0.3
+
+class Game():
+    def __init__(self):
+        self.Map = {
+            'width': 13,
+            'height': 13,
+            'buf': 12,
+            'obj': [
+                # 0: 空氣, 1: 不可炸障礙物, 2: 炸彈, 3: 火焰
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ]
+        }
+        self.players = []
+        self.bombs = []
+        for y in range(1, self.Map.height-1):
+            for x in range(1, game.Map.width-1):
+                if x == 1 and y == 1:
+                    continue
+                self.Map.obj[y][x] = 1 if randint(1, 4) == 1 else 0
+
+    def addPlayer(self, p1):
+        self.players.append(p1)
+
+    def start_game(self):
+        self.players[0].x = UNIT
+        self.players[0].y = UNIT
+        D = {
+            'Map': self.Map.obj,
+            'player_pos': [(self.players[0].x, self.players[0].y)],
+            'bombs': []
+        }
+        # boardcast_status(D)
+        # send coor(players[0].x, players[0].y) to players[0]
+
+game = Game()
 
 def player_control(D):
-    return 1
+    pid = int(D.player_id)
+    if D.key == 'P':
+        game.bombs(Bomb(pid, idx(game.players[pid].x)*UNIT, idx(game.players[pid].y)*UNIT), t)
+        game.Map.obj[idx(game.players[pid].x)][idx(game.players[pid].y)] = 2
+    elif D.key == 'Dw':
+        game.players[pid].is_moving[0] = 1
+        game.players[pid].is_moving[4] = 1
+    elif D.key == 'Ds':
+        game.players[pid].is_moving[1] = 1
+        game.players[pid].is_moving[4] = 1
+    elif D.key == 'Da':
+        game.players[pid].is_moving[2] = 1
+        game.players[pid].is_moving[4] = 0
+    elif D.key == 'Dd':
+        game.players[pid].is_moving[3] = 1
+        game.players[pid].is_moving[4] = 0
+    elif D.key == 'Uw':
+        game.players[pid].is_moving[0] = 0
+    elif D.key == 'Us':
+        game.players[pid].is_moving[1] = 0
+    elif D.key == 'Ua':
+        game.players[pid].is_moving[2] = 0
+    elif D.key == 'Ud':
+        game.players[pid].is_moving[3] = 0
 
+def valid_position(p, dir):
+    """
+      檢查此移動方向是否合法，並更新玩家的位置
+      0:上、1:下、2:左、3:右
+    """
+
+    if dir == 0:
+        if (
+            game.Map.obj[idx(p.y)-1][idx(p.x)] == 1 and
+            p.y - (idx(p.y)-1)*UNIT < game.Map.unit + 1
+        ):
+            return
+        if (
+            idx(p.x)*UNIT - game.Map.buf < p.x and
+            p.x < idx(p.x)*UNIT + game.Map.buf
+        ):
+            p.x = idx(p.x)*UNIT
+            p.y -= p.speed
+    elif dir == 1:
+        if (
+            game.Map.obj[idx(p.y)+1][idx(p.x)] == 1 and
+            (idx(p.y)+1)*UNIT - p.y < UNIT + 1
+        ):
+            return
+        if (
+            idx(p.x)*UNIT - game.Map.buf < p.x and
+            p.x < idx(p.x)*UNIT + game.Map.buf
+        ):
+            p.x = idx(p.x)*UNIT
+            p.y += p.speed
+    elif dir == 2:
+        if (
+            game.Map.obj[idx(p.y)][idx(p.x)-1] == 1 and
+            p.x - (idx(p.x)-1)*UNIT < UNIT + 1
+        ):
+            return
+        if (
+            idx(p.y)*UNIT - game.Map.buf < p.y and
+            p.y < idx(p.y)*UNIT + game.Map.buf
+        ):
+            p.x -= p.speed
+            p.y = idx(p.y)*UNIT
+    elif dir == 3:
+        if (
+            game.Map.obj[idx(p.y)][idx(p.x)+1] == 1 and
+            (idx(p.x)+1)*UNIT - p.x < UNIT + 1
+        ):
+            return
+        if (
+            idx(p.y)*UNIT - game.Map.buf < p.y and
+            p.y < idx(p.y)*UNIT + game.Map.buf
+        ):
+            p.x += p.speed
+            p.y = idx(p.y)*UNIT
+
+def update():
+    """
+    每1/60秒call一次的function
+    """
+    global t
+    t += 0.01
+
+    # 維護每個玩家
+    for p in game.players:
+        dir = -1
+        if p.is_moving[4]:
+            if p.is_moving[0]:
+                dir = 0
+            if p.is_moving[1]:
+                dir = 1
+        else:
+            if p.is_moving[2]:
+                dir = 2
+            if p.is_moving[3]:
+                dir = 3
+        valid_position(p, dir)
+
+    # 維護每個炸彈
+    for b in game.bombs:
+        # 檢查每顆炸彈的時限
+        if t > b.explode_time and not b.set_fire:
+            b.set_fire = True
+            print('Boom!')
+            bomb_x, bomb_y = idx(b.x), idx(b.y)
+            dir_blocked = [0] * 4
+
+            # 計算火焰要延長幾格
+            for j in range(1, POWER+1):
+
+                if not dir_blocked[0]:
+                    if bomb_y-j >= 0 and game.Map.obj[bomb_y-j][bomb_x] != 1:
+                        game.Map.obj[bomb_y-j][bomb_x] = 3
+                        b.fires.append((bomb_y-j, bomb_x))
+                    else:
+                        dir_blocked[0] = 1
+
+                if not dir_blocked[1]:
+                    if bomb_y+j < game.Map.height and game.Map.obj[bomb_y+j][bomb_x] != 1:
+                        game.Map.obj[bomb_y+j][bomb_x] = 3
+                        b.fires.append((bomb_y+j, bomb_x))
+                    else:
+                        dir_blocked[1] = 1
+
+                if not dir_blocked[2]:
+                    if bomb_x-j >= 0 and game.Map.obj[bomb_y][bomb_x-j] != 1:
+                        game.Map.obj[bomb_y][bomb_x-j] = 3
+                        b.fires.append((bomb_y, bomb_x-j))
+                    else:
+                        dir_blocked[2] = 1
+
+                if not dir_blocked[3]:
+                    if bomb_x+j < game.Map.width and game.Map.obj[bomb_y][bomb_x+j] != 1:
+                        game.Map.obj[bomb_y][bomb_x+j] = 3
+                        b.fires.append((bomb_y, bomb_x+j))
+                    else:
+                        dir_blocked[3] = 1
+        
+        if t > b.fire_time:
+            for pos in b.fires:
+                game.Map.obj[pos[0]][pos[1]] = 0
+            game.bombs.remove(b)
+
+def getGameState():
+    return {
+        'Map': game.Map.obj,
+        'player_pos': [[p.x, p.y] for p in game.players]
+    }
 
 async def echo(websocket, path):
     print('New_client')
